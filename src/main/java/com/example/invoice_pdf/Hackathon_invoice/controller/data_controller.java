98a297aadf7734b29cc3a271743a7bfb9323c379
Saksham Gulati <saksham.gulati@CTMCM2JK7H.local>
2025-01-23 20:24:22 +0530

    adding controller

---
+++ src/main/java/com/example/invoice_pdf/Hackathon_invoice/controller/data_controller.java
@@ -0,0 +1,8 @@
+package com.example.invoice_pdf.Hackathon_invoice.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class data_controller {
+
+}
