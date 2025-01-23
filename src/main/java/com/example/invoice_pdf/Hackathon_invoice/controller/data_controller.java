b4f28285c488c90e814899f3ad530c53f23015b0
Amol Mittal <amol.mittal@CTMCYXL50M-3.local>
2025-01-23 22:39:55 +0530

    data controller changed

--- src/main/java/com/example/invoice_pdf/Hackathon_invoice/controller/data_controller.java
+++ src/main/java/com/example/invoice_pdf/Hackathon_invoice/controller/data_controller.java
@@ -1,13 +1,24 @@
 package com.example.invoice_pdf.Hackathon_invoice.controller;
 
+import com.example.invoice_pdf.Hackathon_invoice.dto.Grn;
+import lombok.Data;
 import org.springframework.web.bind.annotation.*;
 
+@Data
 @RestController
+@RequestMapping("/api/grn")
 public class data_controller {
 
-    @PostMapping("/")
-    public String hello(@RequestBody String requestData){
-        return String.format("the data is: %s",requestData);
+    @PostMapping("")
+    public void grnAPI(@RequestBody Grn grn){
+        System.out.println("Received GRN object: " + grn);
+
+        // Example: Print details
+        System.out.println("GRN ID: " + grn.getGrnId());
+        System.out.println("Vendor: " + grn.getVendor().getVendorName());
+        System.out.println("Line Items: " + grn.getLi());
     }
+
+
 
 }
--- src/main/java/com/example/invoice_pdf/Hackathon_invoice/dto/Grn.java
+++ src/main/java/com/example/invoice_pdf/Hackathon_invoice/dto/Grn.java
@@ -1,13 +1,18 @@
 package com.example.invoice_pdf.Hackathon_invoice.dto;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.*;
 
+
 @Data
+@Getter
+@Setter
 public class Grn {
-    private String GRNId;
+    private String GrnId;
     private List<GrnLineitem> Li;
     private LocalDateTime updatedAt;
     private LocalDateTime createdAt;
